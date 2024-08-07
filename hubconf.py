677e82b869de8960c198022b180c1984991fb45e
Jordan Wilke <wilke18@mit.edu>
2024-08-07 14:51:33 -0400

    File path isn't working

--- hubconf.py
+++ hubconf.py
@@ -21,7 +21,7 @@ ljspeech_url = 'https://github.com/wilke0818/StyleTTS2-TorchHub/releases/downloa
 
 def styletts2(progress=True, device=None, 
               pretrain_data: Literal['LibriTTS', 'LJSpeech'] ='LibriTTS') -> StyleTTS2:
-    config = yaml.safe_load(open(f"Models/{pretrain_data}/config.yml"))
+    config = yaml.safe_load(open(f"./Models/{pretrain_data}/config.yml"))
     ckpt_path = libri_tts_url if pretrain_data == 'LibriTTS' else ljspeech_url
     params_whole = torch.hub.load_state_dict_from_url(ckpt_path, progress=progress, check_hash=False, map_location="cpu")
     if device is None: device = 'cuda' if torch.cuda.is_available() else 'cpu'
